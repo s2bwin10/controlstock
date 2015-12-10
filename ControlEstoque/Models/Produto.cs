46c0ceb2ef37044dc086c7b82f255c8a0684c930
senaidepmodas <instrutor.romulo@gmail.com>
2015-12-10 13:19:10 -0200

    Initial Commit

---
+++ ControlEstoque/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlEstoque.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public virtual ICollection<Pedido> Pedidos { get; set; }
+    }
+}
